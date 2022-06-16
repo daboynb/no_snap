5cda4b9a4da7e1440171c7d4dfdd82ed93a40110
daboynb <106079917+daboynb@users.noreply.github.com>
2022-06-16 08:13:00 +0200

    Update no_snap_synaptic.sh

--- no_snap_synaptic.sh
+++ no_snap_synaptic.sh
@@ -5,7 +5,7 @@
 
 # Start
 sudo apt update
-sudo apt-get install dbus-x11 -y
+sudo apt install dbus-x11 -y
 
 echo "Removing snap...This will take a while"
     
@@ -13,8 +13,8 @@ echo "Removing snap...This will take a while"
     sudo systemctl stop snapd && sudo systemctl disable snapd
 
     # Uninstall
-    sudo apt-get purge -y snapd 
-    sudo apt-get purge -y gnome-software-plugin-snap 
+    sudo apt purge -y snapd 
+    sudo apt purge -y gnome-software-plugin-snap 
 
     # Prevent snap from being reinstalled 
     printf "Package: snapd\nPin: release a=*\nPin-Priority: -10" >> no-snap.pref 
@@ -73,11 +73,11 @@ sudo apt install synaptic -y
 # Update, clean the system and reinstall important packages
 echo "Update, clean the system and reinstall important packages"
 sudo rm -R "/snap" "/var/snap" "/var/lib/snapd" "/var/cache/snapd" "/usr/lib/snapd" "/home/$USER/snap"
-sudo apt-get --dry-run autoremove | grep -Po 'Remv \K[^ ]+'  > ./autoremove.txt
+sudo apt --dry-run autoremove | grep -Po 'Remv \K[^ ]+'  > ./autoremove.txt
 sed '/apport*/d' autoremove.txt > reinstall.txt
 rm autoremove.txt
 sudo apt autoremove --purge -y
-xargs -a reinstall.txt sudo apt-get install -y
+xargs -a reinstall.txt sudo apt install -y
 rm reinstall.txt
 sudo apt upgrade -y 
 
